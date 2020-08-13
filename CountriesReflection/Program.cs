14e1cdf1bb48982d7e2159983442a57b79a3716a
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-13 11:39:31 +0300

    add check of exception and check if private function have topsecretattribute and print mamasEmpire contient

--- CountriesReflection/Program.cs
+++ CountriesReflection/Program.cs
@@ -22,42 +22,44 @@ namespace CountriesReflection
                 string className = countries.Name;
                 Console.WriteLine($"the class name: {className}");
 
-                var d = countries.DeclaredMethods.First().DeclaringType;
+                if (className == "MamasEmpire")
+                {
+                    var countryAtrribute = countries.CustomAttributes.Select(x=>x.ConstructorArguments.First()).ToList().First();
+                    Console.WriteLine($"the continent is : {countryAtrribute}");
+
+                }
+
+
                 foreach (var item in countries.DeclaredMethods)
                 {
-                    Console.WriteLine(item.Name);
                     MethodInfo methode = type.GetMethod(item.Name);
-                     
+                  
                     try
                     {
                         var response = methode.Invoke(instance, null);
                         Console.WriteLine($"the { item.Name} is {response}");
-
+                      
                     }
-                    catch(Exception e)
+                    catch(NullReferenceException e)
                     {
-                        Console.WriteLine("the method not return");
+                        Console.WriteLine($"the method {item.Name } not return");
+                    }
+                    catch (TargetParameterCountException e)
+                    {
+                        Console.WriteLine($"the method {item.Name } not return");
                     }
 
 
+                    if (item.IsPrivate == true)
+                    {
+                        if (item.GetCustomAttributes().Where(x => x.GetType().Name == "TopSecretAttribute").Count() > 0)
+                        {
+                            Console.WriteLine($"WOW the country {className} have function {item.Name} that have TopSecretAttribute");
+                        }
+                    }
                 }
 
-               
-
-                
-
-                MethodInfo methodePopulation = type.GetMethod("get_Population");
-                var responsePopulation = methodePopulation.Invoke(instance, null);
-
-                MethodInfo methodeCalculateSize = type.GetMethod("CalculateSize");
-                var responseCalculateSize = methodeCalculateSize.Invoke(instance, null);
-
-             //   Console.WriteLine($"the class name: {className} and the name: {responseName} and the population: {responsePopulation} and the size: {responseCalculateSize}");
-
-
             }
-            
-            Console.WriteLine("Hello World!");
         }
     }
 }
